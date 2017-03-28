e3feb8a8cede281d6b345be486774795112be2d0
YasminLago <yaslagoreb@gmail.com>
2017-03-28 22:36:09 +0200

    Terminado Moving the Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        // A medida que movamos al jugador con el teclado la cámara se mueve a la nueva posición
+        // alineada con el jugador
+        transform.position = player.transform.position + offset;
+    }
+}
